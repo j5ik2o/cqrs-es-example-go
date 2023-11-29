7479720177fb440f624d65b14f251c85faec3dd3
Junichi Kato <j5ik2o@gmail.com>
2023-11-30 01:19:05 +0900

    wip

--- cmd/localRmu.go
+++ cmd/localRmu.go
@@ -23,13 +23,8 @@ import (
 // localRmuCmd represents the localRmu command
 var localRmuCmd = &cobra.Command{
 	Use:   "localRmu",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Read Model Updater for Local",
+	Long:  "Read Model Updater for Local",
 	Run: func(cmd *cobra.Command, args []string) {
 		awsRegion := env.String("", "AWS_REGION")
 		apiHost := env.String("0.0.0.0", "API_HOST")
