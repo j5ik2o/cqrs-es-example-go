e28f02eb744711dc714dfcbdbf45bb842d93fbc1
Junichi Kato <j5ik2o@gmail.com>
2024-01-19 13:53:44 +0900

    refactor: test & add the test

--- pkg/command/interfaceAdaptor/repository/group_chat_repository_test.go
+++ pkg/command/interfaceAdaptor/repository/group_chat_repository_test.go
@@ -10,8 +10,6 @@ import (
 	"github.com/j5ik2o/event-store-adapter-go/common"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/modules/localstack"
 	"testing"
 )
 
@@ -22,32 +20,21 @@ var (
 	snapshotAidIndexName = "snapshot-aid-index"
 )
 
-func TestGroupChatRepositoryImpl_FindById(t *testing.T) {
+func Test_GroupChatRepository_OnDynamoDB_FindById(t *testing.T) {
 	// Given
 	ctx := context.Background()
-	container, err := localstack.RunContainer(
-		ctx,
-		testcontainers.CustomizeRequest(testcontainers.GenericContainerRequest{
-			ContainerRequest: testcontainers.ContainerRequest{
-				Image: "localstack/localstack:2.1.0",
-				Env: map[string]string{
-					"SERVICES":              "dynamodb",
-					"DEFAULT_REGION":        "us-east-1",
-					"EAGER_SERVICE_LOADING": "1",
-					"DYNAMODB_SHARED_DB":    "1",
-					"DYNAMODB_IN_MEMORY":    "1",
-				},
-			},
-		}),
-	)
+
+	container, err := CreateContainer(ctx)
 	require.NoError(t, err)
 	assert.NotNil(t, container)
+
 	dynamodbClient, err := common.CreateDynamoDBClient(t, ctx, container)
 	require.NoError(t, err)
 	assert.NotNil(t, dynamodbClient)
 
 	err = common.CreateJournalTable(t, ctx, dynamodbClient, journalTableName, journalAidIndexName)
 	require.NoError(t, err)
+
 	err = common.CreateSnapshotTable(t, ctx, dynamodbClient, snapshotTableName, snapshotAidIndexName)
 	require.NoError(t, err)
 
@@ -65,6 +52,27 @@ func TestGroupChatRepositoryImpl_FindById(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	repository := NewGroupChatRepository(eventStore)
+	adminId := models.NewUserAccountId()
+	name, err := models.NewGroupChatName("test").Get()
+	if err != nil {
+		t.Fatal(err)
+	}
+	groupChat, event := domain.NewGroupChat(name, adminId, adminId)
+	jsonObj, err := json.Marshal(event.ToJSON())
+	require.NoError(t, err)
+	fmt.Printf("event = %s\n", string(jsonObj))
+	err = repository.StoreEventWithSnapshot(event, groupChat)
+	require.NoError(t, err)
+
+	groupChat2 := repository.FindById(groupChat.GetGroupChatId()).MustGet()
+	require.NotNil(t, groupChat2)
+	assert.Equal(t, groupChat.GetId(), groupChat2.GetId())
+}
+
+func Test_GroupChatRepository_OnMemory_FindById(t *testing.T) {
+	eventStore := esa.NewEventStoreOnMemory()
+
 	repository := NewGroupChatRepository(eventStore)
 	adminId := models.NewUserAccountId()
 	name, err := models.NewGroupChatName("test").Get()
