15842ee9464b439b674ff03e794d8bd93eeb6354
Junichi Kato <j5ik2o@gmail.com>
2024-01-26 09:45:59 +0900

    fix: add const values.

--- pkg/command/domain/events/group_chat_event.go
+++ pkg/command/domain/events/group_chat_event.go
@@ -5,6 +5,16 @@ import (
 	esa "github.com/j5ik2o/event-store-adapter-go"
 )
 
+const (
+	EventTypeGroupChatCreated        = "GroupChatCreated"
+	EventTypeGroupChatDeleted        = "GroupChatDeleted"
+	EventTypeGroupChatRenamed        = "GroupChatRenamed"
+	EventTypeGroupChatMemberAdded    = "GroupChatMemberAdded"
+	EventTypeGroupChatMemberRemoved  = "GroupChatMemberRemoved"
+	EventTypeGroupChatMessagePosted  = "GroupChatMessagePosted"
+	EventTypeGroupChatMessageDeleted = "GroupChatMessageDeleted"
+)
+
 type GroupChatEvent interface {
 	esa.Event
 	GetExecutorId() *models.UserAccountId
